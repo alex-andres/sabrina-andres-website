cdb035b22fcd5a367026f6c19773da2cc1018b9f
c-andres <charlesalexanderandres@gmail.com>
2020-07-20 23:29:24 -0700

    add functioning netlify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,10 +33,12 @@ export default () => (
       >
         <form
           name="contact"
-          method="POST"
+          method="post"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
         >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>
               Your Name: <input type="text" name="name" />
